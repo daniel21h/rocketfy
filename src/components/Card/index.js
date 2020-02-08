4040ac4e966b98ff32f58f1108e408f88d7485db
hesseldan <57191791+hesseldan@users.noreply.github.com>
2020-02-08 08:39:12 -0300

    drag n' drop finished

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,13 +1,16 @@
-import React, { useRef } from 'react';
+import React, { useRef, useContext } from 'react';
 import { useDrag, useDrop } from 'react-dnd'
+
+import BoardContext from '../Board/context'
 
 import { Container, Label } from './styles';
 
-export default function Card({ data }) {
+export default function Card({ data, index, listIndex }) {
   const ref = useRef()
+  const { move } = useContext(BoardContext)
 
   const [{ isDragging }, dragRef] = useDrag({
-    item: { type: 'CARD', id: data.id },
+    item: { type: 'CARD', index, listIndex },
     collect: monitor => ({
       isDragging: monitor.isDragging(),
     }),
@@ -18,11 +21,36 @@ export default function Card({ data }) {
 
     /* Este item, é qual o card está sendo arrastado */
     hover(item, monitor) {
+      const draggedListIndex = item.listIndex
+      const targetListIndex = listIndex
 
-      /* id do card que estou arrastando */
-      console.log(item.id)
-      /*  */
-      console.log(data.id)
+      /* local e altura da troca de posição dentro da mesma lista */
+      const draggedIndex = item.index
+      const targetIndex = index
+      
+      if (draggedIndex === targetIndex && draggedListIndex === targetListIndex) {
+        return
+      }
+
+      const targetSize = ref.current.getBoundingClientRect()
+      const targetCenter = (targetSize.bottom - targetSize.top) / 2
+
+      const draggedOffset = monitor.getClientOffset()
+      const draggedTop = draggedOffset.y - targetSize.top
+
+      if (draggedIndex < targetIndex && draggedTop < targetCenter) {
+        return
+      }
+
+      if (draggedIndex > targetIndex && draggedTop > targetCenter) {
+        return
+      }
+
+      //Definindo a troca
+      move(draggedListIndex, targetListIndex, draggedIndex, targetIndex)
+
+      item.index = targetIndex
+      item.listIndex = targetListIndex
     }
   })
 
--- src/components/List/index.js
+++ src/components/List/index.js
@@ -6,7 +6,7 @@ import Card from '../Card'
 
 import { Container } from './styles';
 
-export default function List({ data }) {
+export default function List({ data, index: listIndex }) {
   return (
     <Container done={data.done}>
       <header>
@@ -14,13 +14,20 @@ export default function List({ data }) {
         {data.creatable && (
           <button type="button">
             <MdAdd sixe={24} color="#fff"/>
-        </button>
+          </button>
         )}
         
       </header>
 
       <ul>
-        { data.cards.map(card => <Card key={card.id} data={card} />)}
+        { data.cards.map((card, index) => (
+        <Card
+          key={card.id}
+          listIndex={listIndex}
+          index={index}
+          data={card} 
+          />
+        ))}
       </ul>
     </Container>
   ); 
